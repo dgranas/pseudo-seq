ed284fc55084224123630c34117c8a9d0bd439b1
duddles <duddles@users.noreply.github.com>
2019-10-07 15:54:44 -0500

    added get_end_count function

--- ribomethseq.py
+++ ribomethseq.py
@@ -18,8 +18,40 @@ def get_end_count(bamfile):
     Given: bamfile for a particular sample
     Gets 5' end of R1 (upstream) reads and 3' of R2 (downstream) reads 
     '''
-    pass
-    # to do
+    samfile = pysam.AlignmentFile(bamfile, 'rb')
+    reads = samfile.fetch()
+    
+    print(bamfile)
+    genes = set()
+    gene_to_5p = {}
+    gene_to_3p = {}
+    
+    for read in reads:
+        gene = read.reference_name
+        if gene not in genes:
+            genes.add(gene)
+            gene_to_5p[gene] = collections.defaultdict(int)
+            gene_to_3p[gene] = collections.defaultdict(int)
+            
+        start_pos = read.reference_start + 1 # add 1 since pysam is 0-based
+        '''
+        from Birkedal et al:
+        The nucleotides at the 3′ read‐ends directly depend on their 2′‐OH function, 
+        whereas the nucleotides at the 5′ read‐ends result from the 2′‐OH function of 
+        their 5′ neighbors. Thus, the 5′ read‐ends are all shifted one position upstream 
+        in the data treatment such that reads corresponding to a methylated nucleotide 
+        will align in the two datasets.
+        '''
+        start_pos -= 1 # shift start 1bp upstream due to fragmentation chemistry mentioned above
+        stop_pos = read.reference_end
+        
+        if read.is_read1 and not read.is_reverse:
+            gene_to_5p[gene][start_pos] += 1
+        elif read.is_read2 and read.is_reverse:
+            gene_to_3p[gene][stop_pos] += 1
+            
+    print(genes)
+    return gene_to_5p, gene_to_3p
 
 if len(sys.argv) != 3:
     raise SystemError('usage: python ribomethseq.py [bamfile] [output folder]')
